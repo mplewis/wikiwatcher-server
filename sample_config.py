0abc910b28e95f5a098f72b1346672a7369784c2
Matthew Lewis <matt@mplewis.com>
2014-01-16 02:08:28 -0800

    Remove wiki scraping config

--- sample_config.py
+++ sample_config.py
@@ -4,16 +4,6 @@
 from peewee import SqliteDatabase
 
 
-class WikiConfig:
-    """
-    Used to configure the MediaWiki API URL as well as the User for logging in,
-    if necessary.
-    """
-    site = 'http://example.com/wiki/api.php'
-    login = {'username': 'BenderBendingRodriguez',
-             'password': 'PleaseInsertGirder'}
-
-
 class DatabaseConfig:
     """Sets the database storage for scraped objects."""
     peewee_db = SqliteDatabase('wikidata.db')
